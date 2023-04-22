df669bd2c67dbd82cd531ad99a73670a0bd58868
Ekaterina Pabauskaya <Katiunya0103@gmail.com>
2023-04-22 18:19:39 +0300

    The task 22 added

--- task_22.py
+++ task_22.py
@@ -9,3 +9,19 @@
 # 2 4 6 8 10 12 10 8 6 4 2
 # 3 6 9 12 15 18
 # 6 12
+
+n = int(input("Enter the number of elements of the first set: "))
+m = int(input("Enter the number of elements of the second set: "))
+
+first = set()
+for i in range(n):
+    first.add(int(input("Enter the element of the first set: ")))
+second = set()    
+for i in range(m):
+    second.add(int(input("Enter the element of the second set: ")))
+
+result = first & second
+
+newResult = list(result)
+#newResult.sort()
+print(*newResult)
